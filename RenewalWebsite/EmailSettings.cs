fc5dbc538c57a9aa1518dfebbdff980b4091ecee
Girish Kolte <girishkolte2000@hotmail.com>
2017-09-01 12:52:11 +0530

    added username field instead of using fromemail so from email can be changed to anything else.

--- RenewalWebsite/EmailSettings.cs
+++ RenewalWebsite/EmailSettings.cs
@@ -8,6 +8,7 @@ namespace RenewalWebsite
     public class EmailSettings
     {
         public string FromEmail { get; set; }
+        public string UserName { get; set; }
         public string Password { get; set; }
         public int Port { get; set; }
         public string Host { get; set; }
