25e888dbe2cc2805b13d32254b805c5395df7e0a
Girish Kolte <girishkolte2000@hotmail.com>
2017-12-04 12:47:26 +0530

    changes for migration files

--- RenewalWebsite/Data/Migrations/20170922082511_EventLog.Designer.cs
+++ RenewalWebsite/Data/Migrations/20170922082511_EventLog.Designer.cs
@@ -8,11 +8,11 @@ using Microsoft.EntityFrameworkCore.Storage.Internal;
 using RenewalWebsite.Data;
 using System;
 
-namespace RenewalWebsite.Migrations
+namespace RenewalWebsite.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20171202063327_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20170922082511_EventLog")]
+    partial class EventLog
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
@@ -205,10 +205,6 @@ namespace RenewalWebsite.Migrations
 
                     b.Property<decimal?>("DonationAmount");
 
-                    b.Property<bool>("IsCustom");
-
-                    b.Property<string>("Reason");
-
                     b.Property<int>("SelectedAmount");
 
                     b.Property<DateTime?>("TransactionDate");
@@ -227,15 +223,19 @@ namespace RenewalWebsite.Migrations
             modelBuilder.Entity("RenewalWebsite.Models.EventLog", b =>
                 {
                     b.Property<int>("Id")
-                        .ValueGeneratedOnAdd();
+                        .ValueGeneratedOnAdd()
+                        .HasColumnName("ID");
 
                     b.Property<DateTime?>("CreatedTime");
 
-                    b.Property<int?>("EventId");
+                    b.Property<int?>("EventId")
+                        .HasColumnName("EventID");
 
-                    b.Property<string>("LogLevel");
+                    b.Property<string>("LogLevel")
+                        .HasMaxLength(50);
 
-                    b.Property<string>("Message");
+                    b.Property<string>("Message")
+                        .HasMaxLength(4000);
 
                     b.HasKey("Id");
 
---
+++ RenewalWebsite/Data/Migrations/20171204071235_donations.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace RenewalWebsite.Data.Migrations
+{
+    public partial class donations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EventID",
+                table: "EventLog",
+                newName: "EventId");
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "EventLog",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "EventLog",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 4000,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LogLevel",
+                table: "EventLog",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCustom",
+                table: "Donations",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Reason",
+                table: "Donations",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCustom",
+                table: "Donations");
+
+            migrationBuilder.DropColumn(
+                name: "Reason",
+                table: "Donations");
+
+            migrationBuilder.RenameColumn(
+                name: "EventId",
+                table: "EventLog",
+                newName: "EventID");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "EventLog",
+                newName: "ID");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "EventLog",
+                maxLength: 4000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LogLevel",
+                table: "EventLog",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
