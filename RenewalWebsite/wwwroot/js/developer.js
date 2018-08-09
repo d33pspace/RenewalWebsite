6edc17639b61101de6d78d1d43e94844e6357547
Girish Kolte <girishkolte2000@hotmail.com>
2018-08-09 15:25:04 +0530

    bug fixes

---
+++ RenewalWebsite/wwwroot/js/developer.js
@@ -0,0 +1,26 @@
+﻿function ConfirmMsg(id, triggerClick, title, message) {
+    $("#" + triggerClick).attr("data-id", id);
+
+    $.confirm({
+        title: title,
+        content: message,
+        theme: 'modern',
+        type: 'orange',
+        typeAnimated: true,
+        closeIcon: true,
+        columnClass: 'jconfirm-small',
+        buttons: {
+            tryAgain: {
+                text: 'Confirm',
+                btnClass: 'btn-red',
+                action: function () {
+                    document.getElementById(triggerClick).click();
+                }
+            },
+            cancel: function () {
+            }
+        }
+    });
+
+    return this;
+};
